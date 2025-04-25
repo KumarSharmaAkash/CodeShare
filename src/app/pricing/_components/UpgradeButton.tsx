53533b3ae2a8448882046d59fa92abbbdadd82e2
saurabh <129882041+KumarSharmaAkash@users.noreply.github.com>
2025-04-25 17:10:29 +0530

    first-V

---
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -0,0 +1,19 @@
+import { Zap } from "lucide-react";
+import Link from "next/link";
+
+export default function UpgradeButton() {
+  const CHEKOUT_URL =
+    "https://codeshare-akash.lemonsqueezy.com/buy/d96e6306-41c1-431b-9ee2-4c0ac2f213af";
+
+  return (
+    <Link
+      href={CHEKOUT_URL}
+      className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white 
+        bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg 
+        hover:from-blue-600 hover:to-blue-700 transition-all"
+    >
+      <Zap className="w-5 h-5" />
+      Upgrade to Pro
+    </Link>
+  );
+}
